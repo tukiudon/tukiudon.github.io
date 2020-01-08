254295f7563b19ff1485d44343398a660ba11285
tukiudon <49028389+tukiudon@users.noreply.github.com>
2020-01-09 02:09:42 +0900

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 /*問題文配列*/
 var sentence = new Array();
-sentence[0] = [ ["<img src='img/20191211_mondai1_1.png'>","<img src='img/20191211_mondai1_2.png'>","<img src='img/20191211_mondai1_3.png'>"]/*"あなたはクラスが一緒で時々話をするAちゃんから下記のようなLINEが来ました。「いきなりごめんね～今時間ある？」ちょうど暇だったこともあり、「あるよ」とあなたは答えました。すると・・・「近くのコンビニでiTunesのプリペイドカード５０００円分買ってきてほしい～🙏」と来ました。Aちゃんはよくゲームに課金をしている子でした。今必要だけど諸事情で行けないから頼んできたんだなと思いました。しかしここでふと、「最近世間で騒がれているLINEの乗っ取り詐欺なのではないか」とあなたは考えました。あなたが優先してとるべき行動は次の３つのうちどれ？"*/,
+sentence[0] = [ ["<img src='img/20191218_mondai1_1.png'>","<img src='img/20191218_mondai1_2.png'>","<img src='img/20191218_mondai1_3.png'>"],
 "①そのままAちゃんと会話をして乗っ取り詐欺か確かめる",
 "②電話やメールなどLINE以外の手段でこのことを聞く",
 "③気になったが、深く考えずプリペイドカードを買って写真を送る。",/*解答*/
@@ -10,7 +10,7 @@ sentence[0] = [ ["<img src='img/20191211_mondai1_1.png'>","<img src='img/2019121
 "正解。LINE以外の連絡手段がとれる場合はそちらできちんと聞きましょう。また、可能であれば一時的にブロックをしておくと安全です。",
 "不正解。１番やってはいけない行為です。詐欺の被害にあってしまいます。"];
 sentence[1] = [
- ["<img src='img/20191211_mondai3_1.png'>","<img src='img/20191211_mondai3_2.png'>"],
+ ["<img src='img/20191218_mondai3_1.png'>","<img src='img/20191218_mondai3_2.png'>"],
 "①メッセージを無視する。",
 "②一度○△銀行にメールなどで問い合わせをし、本当かどうか聞く。",
 "③使えないと困るのでURLから飛んで、口座再開の手続きをする。",
@@ -20,7 +20,7 @@ sentence[1] = [
 "不正解。",
 "正解。本来銀行の暗証番号などが必要になることをSMSなどで聞いてくることはありえません。"];
 sentence[2] = [
-    ["<img src='img/20191211_mondai5.png'>"]
+["<img src='img/20191218_mondai5.png'>"]
  ,
 "①匿名性が高く、Dちゃんが書いたとバレない",
 "②ネット上で他人を誹謗中傷する行為は、犯罪に当たる場合が多い。",
@@ -29,7 +29,27 @@ sentence[2] = [
 "<img src='img/explanation2.png'>",
 "不正解。",
 "正解。法律違反として名誉棄損で刑事告訴される可能性があります。",
+"不正解。"]
+sentence[3] = [
+["<img src='img/20191218_mondai4.PNG'>"],
+"①10歳",
+"②13歳",
+"③16歳",
+2,
+"<img src='img/explanation3.png'>",
+"不正解。",
+"正解。17歳未満の場合でも、iOS機能でアプリの年齢制限をかけていると使用できない場合があります。",
 "不正解。"];
+sentence[4] = [
+["<img src='img/20191218_mondai6_1.PNG'>","<img src='img/20191218_mondai6_2.PNG'>","<img src='img/20191218_mondai6_3.PNG'>"],
+"①個人情報保護法",
+"②著作権、肖像権の侵害",
+"③肖像権の侵害",
+2,
+"<img src='img/explanation4.png'>",
+"不正解。個人情報保護委員会より個人情報を扱うすべての「事業者」に対して個人情報の取り扱いルールを定めたものなので今回は関係がない。「事業者」には、法人に限らず、マンションの管理組合、NPO 法人、自治会や同窓会などの非営利組織も含まれます。 ",
+"不正解。思想又は感情を創作的に表現したものであって、文芸、学術、美術又は音楽の範囲に属するものをいう。 要約するとオリジナルでなにか作ったもの全般のことを言います。今回の写真にも著作権存在がする「可能性」があります。しかしその写真をOB以外の他者が利用したことによるトラブルではないため侵害したことにはならない。",
+"正解。みだりに他人から写真を撮影されたり、それを公表されたりしないよう誰に対しても主張できる権利のことです。撮影の拒絶、公表の拒絶、パブリシティ権の３つが含まれていると考えられます。"];
 
 /*選択師配列*/
 /*var choise = new Array();
@@ -47,7 +67,6 @@ answer = [
 
 
 /*初期設定*/
-var datalist = new Array();
 var cur;/*問題数のカウント*/
 var count;/*問題ページのカウント*/  
 var ansers;/*解答を入れる配列の準備*/
@@ -292,17 +311,10 @@ twi.innerHTML = "<a href='http://twitter.com/share?url=" + url + "&text=わた�
 
 };
 
-var count = 0;
-
 function saveData(){
-
-datalist.push(result);
-    
-    
-count += 1;
-    
-var setjson = JSON.stringify(datalist);
-localStorage.setItem("datalist" + count , setjson);// -> "[object Object]"
+var datalist = new Array();   
+var setjson = JSON.stringify(result);
+localStorage.setItem("key" , setjson);// -> "[object Object]"
 
  // -> {"data1":"hoge1", "data2":"hoge2"}
 
